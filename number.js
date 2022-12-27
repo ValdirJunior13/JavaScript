f4c216b1c62762760a3d648e19a83439178e0917
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2022-12-27 19:20:48 +0000

    Const e números

---
+++ number.js
@@ -0,0 +1,10 @@
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+
+let myVar = 87;
+myVar ++; //Uso do i++
+
+//let myVar = 11;
+//myVar --;  uso do i--
